cb9e4dbc67541151e986e5146271eb6dd974b2a5
Zaid Parkar <zaids.parkar@gmail.com>
2023-04-14 10:22:20 +0530

    admin UI

--- admin/reg_admin.php
+++ admin/reg_admin.php
@@ -37,8 +37,8 @@
 }
 </style>
     <head>
-        <link href="img/hospitallogo.webp" rel="icon">
-        <link rel="stylesheet" href="css2/style.css">
+        <link href="../img/hospitallogo.webp" rel="icon">
+        <link rel="stylesheet" href="../css2/style.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
         <meta charset="UTF-8">
@@ -58,41 +58,33 @@
     srcset="img/dy_mlogo.svg"  style="position: absolute; width: 100vw;height: auto;margin-top: 10%;">
   
   <img 
-    src="img/dy_header_2.svg" 
+    src="../img/dy_header_2.svg" 
     alt="dy_logo"  style="position: absolute; width: 100vw;height: auto;margin-top: 1%;">
 </picture>-->
-<!-- <img src="" style="position: absolute; width: 100vw;height: auto;margin-top: 0%;"> -->
+<!-- <img src="../" style="position: absolute; width: 100vw;height: auto;margin-top: 0%;"> -->
 </nav>
         <div class="text-center parent_div">
 
             <div class="row child_div">
                 <p style="margin-bottom: 3rem;"><b class="MAIN-HEAD"></b></p> 
-                <div class="col-4 ">
+                <div class="col-6 ">
                     <div class="card shadow " style="width: 18rem;">
-                        <img src="img/patientgfg.png" class="card-img-top" alt="..." style="height: 18rem;">
+                        <img src="../img/patientgfg.png" class="card-img-top" alt="..." style="height: 18rem;">
                         <div class="card-body">
 
-                            <a href="user/stu_login.php" class="btn btn-primary">PATIENT LOGIN</a>
+                            <a href="../user/stu_login.php" class="btn btn-primary">Regestir</a>
                         </div>
                     </div>
                 </div>
-                <div class="col-4">
+                <div class="col-6">
                     <div class="card shadow " style="width: 18rem;">
-                        <img src="img/admin_logo.webp" class="card-img-top" alt="..." style="height: 18rem;">
+                        <img src="../img/admin_logo.webp" class="card-img-top" alt="..." style="height: 18rem;">
                         <div class="card-body ">
-                            <a href="admin/admin_login.php" class="btn btn-primary ">Doctor LOGIN</a>
+                            <a href="../admin/admin_login.php" class="btn btn-primary ">Already registered</a>
                         </div>
                     </div>
                 </div>
-                 <div class="col-4">
-                    <div class="card shadow " style="width: 18rem;">
-                        <img src="img/admin_logo.webp" class="card-img-top" alt="..." style="height: 18rem;">
-                        <div class="card-body ">
-                            <a href="admin/reg_admin.php" class="btn btn-primary ">ADMIN LOGIN</a>
-                            <img src="" alt="">
-                        </div>
-                    </div>
-                </div>
+               
             </div>
         </div>
         <script>
